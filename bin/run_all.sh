6eee5b56a0da499a61a71f9eb6a59e674349024a
Dave Pacheco <dap@joyent.com>
2013-07-28 12:04:43 -0700

    polish run_all script a bit more

--- bin/run_all.sh
+++ bin/run_all.sh
@@ -19,7 +19,12 @@ function usage
 usage: $arg0 BIN_DIRECTORY VIDEO_DIRECTORY OUTPUT_DIRECTORY
 
 Runs the whole kartlytics pipeline on all videos in VIDEO_DIRECTORY, placing the
-results into OUTPUT_DIRECTORY.
+results into OUTPUT_DIRECTORY.  BIN_DIRECTORY is used to hold the assets.
+
+You can generate your own results from the public set of videos using:
+
+    $arg0 /$MANTA_USER/stor/kartlytics_bin /dap/public/kartlytics/videos \\
+        /$MANTA_USER/stor/kartlytics_out
 EOF
 	exit 2
 }
@@ -30,6 +35,7 @@ set -o pipefail
 
 ra_jobbasedir="$(dirname $0)/../jobs"
 ra_tarball="/dap/public/kartlytics/kartlytics.tgz"
+ra_binroot="$1"
 
 echo "Uploading assets: "
 for file in $(ls -1 "$ra_jobbasedir"); do
@@ -38,26 +44,26 @@ done
 
 echo "Running job to process videos:"
 echo -n | mjob create -w \
-    -s /dap/public/kartlytics/bin/find-videos \
-    -r "/assets/dap/public/kartlytics/bin/find-videos $2 | xargs mcat" \
-    -s /dap/public/kartlytics/bin/video-transcribe \
-    -s /dap/public/kartlytics/kartlytics.tgz \
+    -s $ra_binroot/find-videos \
+    -r "/assets$ra_binroot/find-videos $2 | xargs mcat" \
+    -s $ra_binroot/video-transcribe \
+    -s $ra_tarball \
     --init "cd /var/tmp && tar xzf /assets$ra_tarball" \
-    -m "/assets/dap/public/kartlytics/bin/video-transcribe /var/tmp/kartlytics $3 "'$MANTA_INPUT_FILE' || \
+    -m "/assets$ra_binroot/video-transcribe /var/tmp/kartlytics $3 "'$MANTA_INPUT_FILE' || \
     fail "failed to process videos"
-
-echo "Running job to aggregate data:"
-echo -n | mjob create -w \
-    -s /dap/public/kartlytics/bin/find-metadata \
-    -r "/assets/dap/public/kartlytics/bin/find-metadata $2 | xargs mcat" \
-    -s /dap/public/kartlytics/bin/video-metadata \
-    -m "/assets/dap/public/kartlytics/bin/video-metadata $3 "'$MANTA_INPUT_FILE' \
-    -r "json -g | mpipe $3/summary.json" || fail "failed to aggregate data"
 
 echo "Running job to generate webms:"
 echo -n | mjob create -w \
-    -s /dap/public/kartlytics/bin/find-videos \
-    -r "/assets/dap/public/kartlytics/bin/find-videos $2 | xargs mcat" \
-    -s /dap/public/kartlytics/bin/video-webm \
-    -m "/assets/dap/public/kartlytics/bin/video-webm $3 "'$MANTA_INPUT_FILE' || \
+    -s $ra_binroot/find-videos \
+    -r "/assets$ra_binroot/find-videos $2 | xargs mcat" \
+    -s $ra_binroot/video-webm \
+    -m "/assets$ra_binroot/video-webm $3 "'$MANTA_INPUT_FILE' || \
     fail "failed to generate webms"
+
+echo "Running job to aggregate data:"
+echo -n | mjob create -w \
+    -s $ra_binroot/find-metadata \
+    -r "/assets$ra_binroot/find-metadata $2 | xargs mcat" \
+    -s $ra_binroot/video-metadata \
+    -m "/assets$ra_binroot/video-metadata $3 "'$MANTA_INPUT_FILE' \
+    -r "json -g | mpipe $3/summary.json" || fail "failed to aggregate data"
