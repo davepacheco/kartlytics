383688b7f8dee2ecde36e4d990b4e9622424436d
Dave Pacheco <dap@joyent.com>
2013-09-06 18:11:55 -0700

    refer to full video set in /manta

--- www/resources/js/config.js
+++ www/resources/js/config.js
@@ -2,6 +2,6 @@
  * kartlytics client configuration
  */
 var kUrlBaseManta = 'http://us-east.manta.joyent.com/manta/public/kartlytics';
-var kUrlBaseVideos = kUrlBaseManta + '/test-videos';
-var kUrlBaseData = kUrlBaseManta + '/out';
+var kUrlBaseVideos = kUrlBaseManta + '/videos';
+var kUrlBaseData = kUrlBaseManta + '/out_full';
 var kUrlSummary = kUrlBaseData + '/summary.json';
