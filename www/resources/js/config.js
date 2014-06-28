ca3543dc824393ef851963b830e897b7134fd2e7
Dave Pacheco <dap@cs.brown.edu>
2014-06-27 20:41:42 -0700

    update default config to most recent output

--- www/resources/js/config.js
+++ www/resources/js/config.js
@@ -1,7 +1,7 @@
 /*
  * kartlytics client configuration
  */
-var kUrlBaseManta = 'http://us-east.manta.joyent.com/manta/public/kartlytics';
+var kUrlBaseManta = 'http://us-east.manta.joyent.com/dap/public/kartlytics';
 var kUrlBaseVideos = kUrlBaseManta + '/videos';
-var kUrlBaseData = kUrlBaseManta + '/out_full';
+var kUrlBaseData = kUrlBaseManta + '/out';
 var kUrlSummary = kUrlBaseData + '/summary.json';
