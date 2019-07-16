3e7e443a9c9f889e37c2e51c0cd31f1d758047e3
David Pacheco <dap@joyent.com>
2019-07-16 14:20:43 -0700

    moved Manta resources (videos, outputs, etc.) to S3

--- www/resources/js/config.js
+++ www/resources/js/config.js
@@ -1,7 +1,7 @@
 /*
  * kartlytics client configuration
  */
-var kUrlBaseManta = 'http://us-east.manta.joyent.com/dap/public/kartlytics';
+var kUrlBaseManta = 'http://kartlytics.s3.us-east-1.amazonaws.com/prime';
 var kUrlBaseVideos = kUrlBaseManta + '/videos';
 var kUrlBaseData = kUrlBaseManta + '/out';
 var kUrlSummary = kUrlBaseData + '/summary.json';
